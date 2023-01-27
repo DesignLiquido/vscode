ea712cf9007fca1eb178a383a4440bece009544f
Samuel Vaz <samuel.rvg@gmail.com>
2023-01-27 11:11:49 -0300

    Funções nativas

--- fontes/completude/delegua-provedor-completude.ts
+++ fontes/completude/delegua-provedor-completude.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import { primitivasNumero } from './primitivas-numero';
+import { primitivasTexto } from './primitivas-texto';
+import { primitivasVetor } from './primitivas-vetor';
 
 /**
  * Classe de provedor de completude de Delégua. 
@@ -7,24 +10,12 @@ import * as vscode from 'vscode';
  * ponto e Ctrl + espaço.
  */
 export class DeleguaProvedorCompletude implements vscode.CompletionItemProvider {
-    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionList<vscode.CompletionItem> | vscode.CompletionItem[]> {
-        const aleatorio = new vscode.CompletionItem(
-            'aleatorio',
-            vscode.CompletionItemKind.Function
-        );
-        aleatorio.documentation =
-            'Retorna um número aleatório entre 0 e 1.';
 
-        return [
-            aleatorio,
-            new vscode.CompletionItem(
-                'aleatorioEntre',
-                vscode.CompletionItemKind.Function
-            ),
-            new vscode.CompletionItem(
-                'texto',
-                vscode.CompletionItemKind.Function
-            ),
-        ];
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionList<vscode.CompletionItem> | vscode.CompletionItem[]> {
+        return [...primitivasNumero, ...primitivasTexto, ...primitivasVetor].map(funcaoNativa => {
+            let completionItem = new vscode.CompletionItem(funcaoNativa.nome, vscode.CompletionItemKind.Function)
+            completionItem.documentation = funcaoNativa.documentacao;
+            return completionItem;
+        })
     }
 }
---
+++ fontes/completude/primitivas-vetor.ts
@@ -0,0 +1,14 @@
+export const primitivasVetor = [
+    {
+        nome: 'mapear',
+        documentacao: 'Percorre um vetor executando uma função para cada item desse mesmo vetor.'
+    },
+    {
+        nome: 'ordenar',
+        documentacao: 'Ordena valores em ordem crescente. Esta função só aceita vetores.'
+    },
+    {
+        nome: 'tamanho',
+        documentacao: 'Retorna o número de elementos que compõem um vetor.'
+    },
+]
