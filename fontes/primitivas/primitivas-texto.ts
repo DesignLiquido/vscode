2e2571f01b5046e54402ca71b22b7294d5d051b2
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2023-02-12 10:10:37 -0800

    Atualização das priitivas de texto e vetor.

--- fontes/primitivas/primitivas-texto.ts
+++ fontes/primitivas/primitivas-texto.ts
@@ -1,5 +1,17 @@
 export const primitivasTexto = [
     {
+        nome: 'dividir',
+        documentacao: 'Divide o texto pelo separador passado como parâmetro.'
+    },
+    {
+        nome: 'fatiar',
+        documentacao: 'Extrai uma fatia do texto, dadas posições de início e fim.'
+    },
+    {
+        nome: 'inclui',
+        documentacao: 'Devolve verdadeiro se elemento passado por parâmetro está contido no texto, e falso em caso contrário.'
+    },
+    {
         nome: 'maiusculo',
         documentacao: 'Converte todos os caracteres alfabéticos para maiúsculas.'
     },
@@ -8,7 +20,15 @@ export const primitivasTexto = [
         documentacao: 'Converte todos os caracteres alfabéticos para minúsculas.'
     },
     {
-        nome: 'texto',
-        documentacao: 'Transforma números flutuantes ou inteiros em texto.'
+        nome: 'substituir',
+        documentacao: 'Substitui a primeira ocorrência no texto do primeiro parâmetro pelo segundo parâmetro.'
     },
-]
+    {
+        nome: 'subtexto',
+        documentacao: 'Extrai uma fatia do texto, dadas posições de início e fim.'
+    },
+    {
+        nome: 'tamanho',
+        documentacao: 'Devolve um número inteiro com o número de caracteres do texto.'
+    }
+];
--- fontes/primitivas/primitivas-vetor.ts
+++ fontes/primitivas/primitivas-vetor.ts
@@ -1,14 +1,50 @@
 export const primitivasVetor = [
     {
-        nome: 'mapear',
-        documentacao: 'Percorre um vetor executando uma função para cada item desse mesmo vetor.'
+        nome: 'adicionar',
+        documentacao: 'Adiciona um elemento ao final do vetor.'
+    },
+    {
+        nome: 'empilhar',
+        documentacao: 'Adiciona um elemento ao final do vetor.'
+    },
+    {
+        nome: 'fatiar',
+        documentacao: 'Extrai uma fatia do vetor, dadas posições de início e fim.'
+    },
+    {
+        nome: 'inclui',
+        documentacao: 'Devolve verdadeiro se elemento passado por parâmetro está contido no vetor, e falso em caso contrário.'
+    },
+    {
+        nome: 'inverter',
+        documentacao: 'Inverte a ordem dos elementos de um vetor.'
+    },
+    {
+        nome: 'juntar',
+        documentacao: 'Junta os elementos de um vetor em um literal de texto, separando os elementos pelo separados passado como parâmetro.'
     },
     {
         nome: 'ordenar',
         documentacao: 'Ordena valores em ordem crescente. Esta função só aceita vetores.'
     },
     {
-        nome: 'tamanho',
-        documentacao: 'Retorna o número de elementos que compõem um vetor.'
+        nome: 'remover',
+        documentacao: 'Remove um elemento do vetor caso o elemento exista no vetor.'
     },
-]
+    {
+        nome: 'removerPrimeiro',
+        documentacao: 'Remove o primeiro elemento do vetor caso o elemento exista no vetor.'
+    },
+    {
+        nome: 'removerUltimo',
+        documentacao: 'Remove o último elemento do vetor caso o elemento exista no vetor.'
+    },
+    {
+        nome: 'somar',
+        documentacao: 'Soma ou concatena todos os elementos do vetor (de acordo com o tipo de dados desses elementos) e retorna o resultado.'
+    },
+    {
+        nome: 'tamanho',
+        documentacao: 'Retorna o número de elementos que compõem o vetor.'
+    },
+];
