c0bfd2aaad8db9a721217c9ff9e908e1431517c3
VitBrandao <vitorsbsilva@gmail.com>
2023-09-15 12:07:02 -0300

    Cria pasta e arquivo do provedor LMHT com objeto-exemplo inicial

---
+++ fontes/linguagens/lmht/estruturas.ts
@@ -0,0 +1,14 @@
+export default {
+  'abreviacao': {
+    nomeHtml: 'abbr',
+    descricao: 'Estrutura utilizada como forma de abreviar uma parte específica do texto (ou criar um acrônimo), seja dentro de uma outra estrutura ou não.',
+    documentacao: '# `abreviacao`\nGeralmente, nos artigos jornalísticos ou até mesmo textos dissertativos, deve-se utilizar a abreviação para que não seja necessário utilizar o termo inteiro, ou para que seja um informativo para o leitor.',
+    exemploCodigo: '<titulo1>\n A Linguagem de Marcação Hiper-Texto <abreviacao> LMHT </abreviacao> foi criada por Leonel \n</titulo1>\n}'
+  },
+  'abreviação': {
+    nomeHtml: 'abbr',
+    descricao: 'Estrutura utilizada como forma de abreviar uma parte específica do texto (ou criar um acrônimo), seja dentro de uma outra estrutura ou não.',
+    documentacao: '# `abreviação`\nGeralmente, nos artigos jornalísticos ou até mesmo textos dissertativos, deve-se utilizar a abreviação para que não seja necessário utilizar o termo inteiro, ou para que seja um informativo para o leitor.',
+    exemploCodigo: '<titulo1>\n A Linguagem de Marcação Hiper-Texto <abreviação> LMHT </abreviação> foi criada por Leonel \n</titulo1>\n}'
+  },
+};
