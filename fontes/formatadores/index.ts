6a023405526e6598bbbfcf66a776f768c3ae4715
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-01-20 17:06:15 -0800

    Atualização de `delegua-node` para a versão 0.30.2, contemplando:
    - Novo analisador semântico para o VisuAlg;
    - Novo formatador para Delégua.

--- fontes/formatadores/index.ts
+++ fontes/formatadores/index.ts
@@ -1,21 +1,31 @@
 import * as vscode from 'vscode';
 import * as sistemaOperacional from 'node:os';
 
-import { lexadores } from '@designliquido/delegua';
+import { avaliadores, lexadores } from '@designliquido/delegua';
 import { FormatadorDelegua } from '@designliquido/delegua/fontes/formatadores';
 
 export class DeleguaProvedorFormatacao implements vscode.DocumentFormattingEditProvider {
     provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
         const lexador = new lexadores.Lexador();
-        const formatador = new FormatadorDelegua();
+        const avaliadorSintatico = new avaliadores.AvaliadorSintatico(false);
+        const formatador = new FormatadorDelegua(sistemaOperacional.EOL);
+
         const resultadoLexador = lexador.mapear(document.getText().split('\n'), -1);
+        const resultadoAvaliacaoSintatica = avaliadorSintatico.analisar(resultadoLexador, -1);
+        let codigoFormatado: string = document.getText();
+        try {
+            codigoFormatado = formatador.formatar(resultadoAvaliacaoSintatica.declaracoes);
+        } catch (erro) {
+            console.error(erro);
+        }
+        
         return [
             vscode.TextEdit.replace(
                 new vscode.Range(
                     document.lineAt(0).range.start,
                     document.lineAt(document.lineCount - 1).range.end
                 ),
-                formatador.formatar(resultadoLexador.simbolos, sistemaOperacional.EOL)
+                codigoFormatado
             ),
         ];
     }
