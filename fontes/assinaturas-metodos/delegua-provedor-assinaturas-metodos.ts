12c25f6ca74cd81dd9b85b45e2f11914244f1d89
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2025-11-12 11:04:32 -0800

    Melhorando forma de sugestão de assinatura de métodos, baseada no tipo da primitiva, ou quando não houver primitiva, no catálogo de funções nativas.

--- fontes/assinaturas-metodos/delegua-provedor-assinaturas-metodos.ts
+++ fontes/assinaturas-metodos/delegua-provedor-assinaturas-metodos.ts
@@ -1,38 +1,19 @@
 import * as vscode from 'vscode';
 
-import { primitivas } from '../bibliotecas';
+import { Const, Var } from '@designliquido/delegua/declaracoes';
+
+import { primitivas, primitivasDicionarioFormatadas, primitivasNumeroFormatadas, primitivasTextoFormatadas, primitivasVetorFormatadas, funcoesNativasDelegua } from '../bibliotecas';
 import { FuncaoNativaOuMetodoPrimitiva } from '../bibliotecas/tipos';
+import { obterResultado } from '../analise-codigo/cache-analise';
 
 /**
  * Provedor de assinatura de métodos de Delégua.
  */
 export class DeleguaProvedorAssinaturaMetodos implements vscode.SignatureHelpProvider {
-    provideSignatureHelp(
-        document: vscode.TextDocument, 
-        position: vscode.Position, 
-        token: vscode.CancellationToken, 
-        context: vscode.SignatureHelpContext
-    ): vscode.ProviderResult<vscode.SignatureHelp> {
-        const regexMetodo = /([a-zA-Z_0-9]+)\(/gi;
-        const intervalo = document.getWordRangeAtPosition(position, regexMetodo);
-        const palavra = document.getText(intervalo);
-        const resultadoRegex = regexMetodo.exec(palavra);
-
-        if (!resultadoRegex) {
-            return undefined;
-        }
-
-        const primitivaOuMetodoGlobal: FuncaoNativaOuMetodoPrimitiva | undefined = primitivas.find(
-            (primitiva) => primitiva.nome === resultadoRegex[1]
-        );
-
-        if (primitivaOuMetodoGlobal === undefined) {
-            return undefined;    
-        }
-
+    protected construirObjetoAssinatura(primitivaOuMetodoGlobal: FuncaoNativaOuMetodoPrimitiva): vscode.SignatureHelp {
         const topicoAjuda = new vscode.SignatureHelp();
         topicoAjuda.signatures = [];
-        
+
         for (let assinatura of primitivaOuMetodoGlobal.assinaturas || []) {
             const assinaturaMetodo = new vscode.SignatureInformation(
                 assinatura.formato, 
@@ -53,5 +34,101 @@ export class DeleguaProvedorAssinaturaMetodos implements vscode.SignatureHelpPro
         }
         
         return topicoAjuda;
+    }
+
+    provideSignatureHelp(
+        document: vscode.TextDocument, 
+        position: vscode.Position, 
+        token: vscode.CancellationToken, 
+        context: vscode.SignatureHelpContext
+    ): vscode.ProviderResult<vscode.SignatureHelp> {
+        const regexMetodo = /([a-zA-Z_0-9]+)\(/gi;
+        const linhaTexto = document.lineAt(position).text;
+        const resultadoRegex = regexMetodo.exec(linhaTexto);
+        const textoAntesCursor = linhaTexto.substring(0, position.character);
+
+        if (!resultadoRegex) {
+            return undefined;
+        }
+
+        const resultadoAnalise = obterResultado(document.uri.toString());
+        const todasAsVariaveisOuConstantes = resultadoAnalise?.avaliadorSintatico.declaracoes.flatMap(declaracao => {
+            if (declaracao instanceof Var) {
+                return [{ nome: declaracao.simbolo.lexema, tipo: declaracao.tipo }];
+            }
+
+            if (declaracao instanceof Const) {
+                return [{ nome: declaracao.simbolo.lexema, tipo: declaracao.tipo }];
+            }
+
+            return [];
+        }) || [];
+
+        const regexObjeto = /([a-zA-Z_0-9]+)\.\s*/;
+        const resultadoObjeto = regexObjeto.exec(textoAntesCursor);
+
+        let tipoObjeto: string | undefined = undefined;
+        let nomeObjeto: string | undefined = undefined;
+        if (resultadoObjeto) {
+            nomeObjeto = resultadoObjeto[1];
+            const variavelOuConst = todasAsVariaveisOuConstantes.find(
+                (decl) => decl.nome === nomeObjeto
+            );
+            tipoObjeto = variavelOuConst?.tipo;
+        }
+
+        // Primeiro caso: A `palavra` é um método de um objeto.
+        if (tipoObjeto) {
+            switch (tipoObjeto) {
+                case 'dicionario':
+                case 'dicionário':
+                    const metodoDicionario = primitivasDicionarioFormatadas.find(m => m.nome === resultadoRegex[1]);
+                    if (metodoDicionario) {
+                        return this.construirObjetoAssinatura(metodoDicionario);
+                    }
+
+                    return undefined;
+                case 'numero':
+                case 'número':
+                    const metodoNumero = primitivasNumeroFormatadas.find(m => m.nome === resultadoRegex[1]);
+                    if (metodoNumero) {
+                        return this.construirObjetoAssinatura(metodoNumero);
+                    }
+
+                    return undefined;
+                case 'texto':
+                    const metodoTexto = primitivasTextoFormatadas.find(m => m.nome === resultadoRegex[1]);
+                    if (metodoTexto) {
+                        return this.construirObjetoAssinatura(metodoTexto);
+                    }
+
+                    return undefined;
+                case 'vetor':
+                    const metodoVetor = primitivasVetorFormatadas.find(m => m.nome === resultadoRegex[1]);
+                    if (metodoVetor) {
+                        return this.construirObjetoAssinatura(metodoVetor);
+                    }
+
+                    return undefined;
+
+                default:
+                    const metodoQualquer = primitivas.find(m => m.nome === resultadoRegex[1]);
+                    if (metodoQualquer) {
+                        return this.construirObjetoAssinatura(metodoQualquer);
+                    }
+
+                    return undefined;
+                }
+        }
+
+        const possivelFuncaoNativa: FuncaoNativaOuMetodoPrimitiva | undefined = funcoesNativasDelegua.find(
+            (funcaoNativa) => funcaoNativa.nome === resultadoRegex[1]
+        );
+
+        if (!possivelFuncaoNativa) {
+            return undefined;    
+        }
+        
+        return this.construirObjetoAssinatura(possivelFuncaoNativa);
     }
 }
