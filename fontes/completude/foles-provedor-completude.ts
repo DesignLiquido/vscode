830060981970f46d0c0331805df08120ad98020f
Vitor Brandão <82619724+VitBrandao@users.noreply.github.com>
2026-06-16 14:12:52 -0300

    Incluindo lista modificadores vinda do repositório FolEs (#95)
    * Atualiza versão de dependência FolEs para 0.16.0
    
    * Exclui lista local de modificadores FolEs
    
    * Inclui lista de modificadores vinda do repositório FolEs
    
    * Acerta workflow para evitar conflito de versões local e integração
    
    * Corrige para formatação original do workflow

--- fontes/completude/foles-provedor-completude.ts
+++ fontes/completude/foles-provedor-completude.ts
@@ -1,13 +1,12 @@
 import * as vscode from 'vscode';
-
-import modificadoresFoles from '../linguagens/foles/modificadores';
+import listaModificadores from '@designliquido/foles/extensao/lista-modificadores';
 
 export class FolesProvedorCompletude implements vscode.CompletionItemProvider {
 
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionList<vscode.CompletionItem> | vscode.CompletionItem[]> 
     {
         const todosModificadores: vscode.CompletionItem[] = [];
-        for (let [chave, valor] of Object.entries(modificadoresFoles)) {
+        for (let [chave, valor] of Object.entries(listaModificadores)) {
             let item = new vscode.CompletionItem(chave, vscode.CompletionItemKind.Property);
             item.documentation = `Equivalente em CSS: ${valor.nomeCss}`;
             todosModificadores.push(item);
